06be4038051e96e070737718f8d0e9124cc008d8
Vluch <Vluch>
2019-05-19 12:15:45 +0300

    section2 added

--- js/main.js
+++ js/main.js
@@ -8,20 +8,3 @@ $('.menu-left a').on('click', function() {
   $('.nav-toggle').removeClass('open');
   $('.menu-left').removeClass('collapse');
 });
-
-// Hide Header on on scroll down
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = $('header').outerHeight();
-
-$(window).scroll(function(event){
-    didScroll = true;
-});
-
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
-}, 250);
